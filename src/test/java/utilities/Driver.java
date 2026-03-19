d126bc2d564e8b9b70680597c99739e9028a469b
ergun.yilmaz <ergunyilmaz176@gmail.com>
2026-03-19 10:53:00 +0300

    driver update-1

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,8 +2,6 @@ package utilities;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.ios.IOSDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -11,12 +9,15 @@ import java.time.Duration;
 public class Driver {
 
     private static AndroidDriver appiumDriver;
-    private static IOSDriver iosDriver;
 
     public static AndroidDriver getAndroidDriver() {
         if (appiumDriver == null) {
             try {
-                URL appiumServerURL = new URL("http://127.0.0.1:4723");
+                // GitHub Actions ortamında mıyız?
+                boolean isGitHubActions = "true".equalsIgnoreCase(System.getenv("GITHUB_ACTIONS"));
+
+                // Appium 2.x için varsayılan URL (Base path gerekmez)
+                URL appiumServerURL = new URL("http://127.0.0.1:4723/");
 
                 UiAutomator2Options options = new UiAutomator2Options();
                 options.setAutomationName("UiAutomator2");
@@ -24,37 +25,28 @@ public class Driver {
                 options.setNoReset(false);
                 options.setAutoGrantPermissions(true);
                 options.setNewCommandTimeout(Duration.ofSeconds(300));
-                options.setCapability("disableIdLocatorAutocompletion", true);
-
-                boolean isGitHubActions = "true".equalsIgnoreCase(System.getenv("GITHUB_ACTIONS"));
 
                 if (isGitHubActions) {
+                    // CI Ortamı Ayarları
                     options.setDeviceName("Android Emulator");
-                    options.setPlatformVersion("11.0");
+                    options.setPlatformVersion("11.0"); // API 30 = Android 11
 
-                    // APK'yı direkt projeden al
+                    // APK dosyasının proje içindeki yolu
                     String appPath = System.getProperty("user.dir") + "/Apps/tb.apk";
                     options.setApp(appPath);
-
                 } else {
+                    // Yerel (Local) Bilgisayar Ayarları
                     options.setDeviceName("Pixel 4 H");
                     options.setPlatformVersion("10.0");
-
                     options.setAppPackage("com.mikatur.tatilbudur");
                     options.setAppActivity("com.mikatur.tatilbudur.MainActivity");
                 }
 
-                if (ConfigReader.getProperty("platformName").equalsIgnoreCase("Android")) {
-                    appiumDriver = new AndroidDriver(appiumServerURL, options);
-                    appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-                } else {
-                    iosDriver = new IOSDriver(appiumServerURL, options);
-                    iosDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-                    throw new UnsupportedOperationException("Cihaz IOS");
-                }
+                appiumDriver = new AndroidDriver(appiumServerURL, options);
+                appiumDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
             } catch (MalformedURLException e) {
-                throw new RuntimeException("Appium server URL hatalı: " + e.getMessage(), e);
+                throw new RuntimeException("Appium sunucu adresi hatalı!", e);
             }
         }
         return appiumDriver;
